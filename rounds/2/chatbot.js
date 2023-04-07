a350a15466f3c961268be8924384c0eaabecf393
Jonatan Groth Frausing <jfr@mft-energy.com>
2023-04-07 10:36:23 +0200

    stuff not working

--- rounds/2/chatbot.js
+++ rounds/2/chatbot.js
@@ -1,3 +1,5 @@
+const { filter } = require("async");
+
 module.exports = function () {
   var info = {
     name: "chatbot",
@@ -13,20 +15,13 @@ module.exports = function () {
     let playersLeft =
       game.players.filter((player) => player.state === "active").length - 1;
 
-    let won = 0;
-    let testRounds = 100;
-    for (let i = 0; i < testRounds; i++) {
-      if (checkWinningHand(cards, game.community, playersLeft)) {
-        won++;
-      }
-    }
-    let winRatio = won / testRounds;
+    let winRatio = getWinRatio(cards, game.community, playersLeft);
     if (game.state !== "complete") {
       if (winRatio > 0.95) {
         return game.self.chips;
       } else if (winRatio > 0.4) {
         return Math.max(game.betting.call, pot * 2 * winRatio);
-      } else if (winRatio > 0.2) {  
+      } else if (winRatio > 0.2) {
         return Math.max(game.betting.call, pot);
       }
 
@@ -34,11 +29,11 @@ module.exports = function () {
         if (winRatio > 0.05 && game.betting.call <= pot) {
           return game.betting.call;
         }
-      } else       if (game.state === "flop") {
+      } else if (game.state === "flop") {
         if (winRatio > 0.1 && game.betting.call <= pot) {
           return game.betting.call;
         }
-      } else       if (game.state === "river") {
+      } else if (game.state === "river") {
         if (winRatio > 0.15 && game.betting.call <= pot) {
           return game.betting.call;
         }
@@ -50,10 +45,6 @@ module.exports = function () {
 
   return { update: update, info: info };
 };
-
-function nOfValue(values, n) {
-  return Object.entries(values).filter(([key, value]) => value === n);
-}
 
 var valueMap = {
   A: 14,
@@ -72,6 +63,35 @@ var valueMap = {
 };
 
 var suits = ["c", "d", "h", "s"];
+const step = Math.pow(10, 10);
+const PAIR = step;
+const TWO_PAIR = 2 * step;
+const THREE_OF_A_KIND = 3 * step;
+const STRAIGHT = 4 * step;
+const FLUSH = 5 * step;
+const FULL_HOUSE = 6 * step;
+const FOUR_OF_A_KIND = 7 * step;
+const STRAIGHT_FLUSH = 8 * step;
+
+function getWinRatio(cards, community, playersLeft) {
+  let won = 0;
+  let testRounds = 1000;
+  for (let i = 0; i < testRounds; i++) {
+    if (checkWinningHand(cards, community, playersLeft)) {
+      won++;
+    }
+  }
+  let winRatio = won / testRounds;
+  return winRatio;
+}
+
+function valueOfCardValues(values) {
+  let top5 = values.slice(0, 5).reverse();
+  return top5.reduce(
+    (acc, val, idx) => acc + valueMap[val] * Math.pow(100, idx),
+    0
+  );
+}
 
 function evaluatePokerHand(hand) {
   // Convert the hand to an array of numerical values
@@ -87,27 +107,21 @@ function evaluatePokerHand(hand) {
   let numericalSuits = suits.reduce(groupFn, {});
 
   // Check for the different types of hands in poker
-  if (isStraightFlush(hand, suits)) {
-    return 90;
-  } else if (isFourOfAKind(numericalGroups)) {
-    return 80;
-  } else if (isFullHouse(numericalGroups)) {
-    return 70;
-  } else if (isFlush(numericalSuits)) {
-    return 60;
-  } else if (isStraight(numericalValues)) {
-    return 50;
-  } else if (isThreeOfAKind(numericalGroups)) {
-    return 40;
-  } else if (isTwoPairs(numericalGroups)) {
-    return 30;
-  } else if (isOnePair(numericalGroups)) {
-    return 20;
-  } else {
-    return numericalValues[numericalValues.length - 1];
-  }
+
+  return (
+    straightFlush(hand, suits) ||
+    fourOfAKind(numericalGroups, numericalValues) ||
+    fullHouse(numericalGroups) ||
+    flush(numericalSuits, hand) ||
+    straight(numericalValues) ||
+    threeOfAKind(numericalGroups, numericalValues) ||
+    twoPairs(numericalGroups, numericalValues) ||
+    onePair(numericalGroups, numericalValues) ||
+    valueOfCardValues(numericalValues.reverse())
+  );
 }
-function isStraightFlush(hand, suits) {
+
+function straightFlush(hand, suits) {
   let frequentSuit = Object.entries(suits).reduce(
     (acc, [key, value]) => {
       if (value > acc[1]) {
@@ -120,30 +134,96 @@ function isStraightFlush(hand, suits) {
   );
 
   if (frequentSuit[1] < 5) {
-    return false;
+    return undefined;
   }
   let cardsOfSuit = hand
     .filter((card) => card[1] === frequentSuit[0])
     .map((card) => valueMap[card[0]])
     .sort((a, b) => a - b);
 
-  return isStraight(cardsOfSuit);
+  let straightVal = straight(cardsOfSuit);
+  if (!straightVal) {
+    return undefined;
+  }
+
+  return straightVal - STRAIGHT + STRAIGHT_FLUSH ;
 }
 
-function isFourOfAKind(numericalGroups) {
-  return Object.values(numericalGroups).includes(4);
+function fourOfAKind(numericalGroups, numericalValues) {
+  // get the 4oak
+  let fourOfAKind = Object.entries(numericalGroups).filter(
+    (pair) => pair[1] === 4
+  );
+
+  if (fourOfAKind.length === 0) {
+    return undefined;
+  }
+
+  // select values from pairs and sort them in descending order
+  let highest4oak = fourOfAKind[0][0];
+
+  // filter the numerical values to not include the pair
+  let filteredValues = numericalValues.filter((value) => value !== highest4oak);
+  // sort the values in descending order
+  filteredValues.sort((a, b) => b - a)[0];
+  // return the sum of the highest pair and the highest 3 cards
+  return (
+    valueOfCardValues([
+      highest4oak,
+      highest4oak,
+      highest4oak,
+      highest4oak,
+      filteredValues[0],
+    ]) + FOUR_OF_A_KIND
+  );
+}
+function fullHouse(numericalGroups) {
+  let threeOfAKinds = Object.entries(numericalGroups)
+    .filter((pair) => pair[1] === 3)
+    .map((pair) => pair[0])
+    .sort((a, b) => b - a);
+  if (threeOfAKinds.length === 0) {
+    return undefined;
+  }
+  let highestThreeOfAKind = threeOfAKinds[0];
+  let pairs = Object.entries(numericalGroups).filter(
+    (pair) => pair[1] >= 2 && pair[0] != highestThreeOfAKind
+  );
+
+  if (pairs.length === 0) {
+    return undefined;
+  }
+
+  // select values from pairs and sort them in descending order
+  let highestPair = pairs.map((pair) => pair[0]).sort((a, b) => b - a)[0];
+
+  return (
+    valueOfCardValues([
+      highestThreeOfAKind,
+      highestThreeOfAKind,
+      highestThreeOfAKind,
+      highestPair,
+      highestPair,
+    ]) + FULL_HOUSE
+  );
 }
 
-function isFullHouse(numericalGroups) {
-  const values = Object.values(numericalGroups);
-  return values.includes(2) && values.includes(3);
+function flush(numericalSuits, hand) {
+  let flushSuit = Object.entries(numericalSuits)
+    .filter(([suit, count]) => count >= 5)
+    .map(([suit, _]) => suit);
+  if (flushSuit.length == 0) {
+    return undefined;
+  }
+  let cardsOfSuit = hand
+    .filter((card) => card[1] === flushSuit[0])
+    .map((card) => valueMap[card[0]])
+    .sort((a, b) => b - a);
+
+  return valueOfCardValues(cardsOfSuit) + FLUSH;
 }
 
-function isFlush(numericalSuits) {
-  return Object.values(numericalSuits).some((count) => count >= 5);
-}
-
-function isStraight(numericalValues_) {
+function straight(numericalValues_) {
   // get distinct values
 
   let numericalValues = numericalValues_.reduce((acc, val) => {
@@ -157,32 +237,102 @@ function isStraight(numericalValues_) {
     numericalValues.push(1);
   }
 
-  numericalValues.sort((a, b) => a - b);
+  numericalValues.sort((a, b) => b - a);
   // Check for a straight with an Ace low
   let straightStart = 0;
   for (let i = 0; i <= numericalValues.length; i++) {
     if (i === straightStart + 4) {
-      return true;
+      return (
+        valueOfCardValues(numericalValues.slice(straightStart, 6)) + STRAIGHT
+      );
     }
-    if (numericalValues[i] + 1 !== numericalValues[i + 1]) {
+    if (numericalValues[i] - 1 !== numericalValues[i + 1]) {
       straightStart = i + 1;
     }
   }
-  return false;
+  return undefined;
 }
 
-function isThreeOfAKind(numericalGroups) {
-  return Object.values(numericalGroups).some((count) => count === 3);
+function threeOfAKind(numericalGroups, numericalValues) {
+  let threeOfAKind = Object.entries(numericalGroups).filter(
+    (pair) => pair[1] === 3
+  );
+
+  if (threeOfAKind.length === 0) {
+    return undefined;
+  }
+
+  //select the highest three of a kind value
+  let highestThreeOfAKind = threeOfAKind
+    .map((threeOfAKind) => threeOfAKind[0])
+    .sort((a, b) => b - a)[0];
+
+  //filter out the three of a kind values
+  let filteredValues = numericalValues.filter(
+    (value) => value !== highestThreeOfAKind
+  );
+
+  //sort the remaining values in descending order
+  filteredValues.sort((a, b) => b - a);
+
+  //return the sum of the three of a kind value and the highest two cards
+  return (
+    valueOfCardValues([
+      highestThreeOfAKind,
+      highestThreeOfAKind,
+      highestThreeOfAKind,
+      ...filteredValues,
+    ]) + THREE_OF_A_KIND
+  );
 }
 
-function isTwoPairs(numericalGroups) {
-  const values = Object.values(numericalGroups);
-  const numPairs = values.filter((count) => count === 2).length;
-  return numPairs === 2;
+function twoPairs(numericalGroups, numericalValues) {
+  let pairs = Object.entries(numericalGroups).filter((pair) => pair[1] === 2);
+  if (pairs.length < 2) {
+    return undefined;
+  }
+
+  // select values from pairs and sort them in descending order
+  let highestPairs = pairs
+    .map((pair) => parseInt(pair[0]))
+    .sort((a, b) => b - a)
+    .slice(0, 2);
+
+  let filteredValues = numericalValues.filter(
+    (value) => !highestPairs.includes(value)
+  );
+
+  filteredValues.sort((a, b) => b - a);
+
+  // return the sum of the highest pair and the highest 3 cards
+  return (
+    valueOfCardValues([
+      highestPairs[0],
+      highestPairs[0],
+      highestPairs[1],
+      highestPairs[1],
+      ...filteredValues,
+    ]) + TWO_PAIR
+  );
 }
 
-function isOnePair(numericalGroups) {
-  return Object.values(numericalGroups).some((count) => count === 2);
+function onePair(numericalGroups, numericalValues) {
+  // get the pairs
+  let pairs = Object.entries(numericalGroups).filter((pair) => pair[1] === 2);
+
+  if (pairs.length === 0) {
+    return undefined;
+  }
+
+  // select values from pairs and sort them in descending order
+  let highestPair = parseInt(pairs.map((pair) => pair[0]).sort((a, b) => b - a)[0]);
+
+  // filter the numerical values to not include the pair
+  let filteredValues = numericalValues.filter((value) => value !== highestPair);
+  // sort the values in descending order
+  filteredValues.sort((a, b) => b - a);
+  // return the sum of the highest pair and the highest 3 cards
+  return valueOfCardValues([highestPair, highestPair, ...filteredValues]) + PAIR;
 }
 
 function generateRandomCards(numCards, deck) {
@@ -216,18 +366,16 @@ function checkWinningHand(myCards, communityCards, numPlayers) {
   const missingCards = 5 - communityCards.length;
   const randomCards = generateRandomCards(missingCards, deck);
   const allCommunityCards = [...communityCards, ...randomCards];
-  deck = deck.filter((card) => !randomCards.includes(card));
+
 
   // Evaluate each player's hand and compare with known cards
   let highestHand = evaluatePokerHand([...myCards, ...allCommunityCards]);
-  for (let i = 0; i < otherPlayersCards.length; i++) {
-    const playerHand = [...otherPlayersCards[i], ...allCommunityCards];
-    const playerScore = evaluatePokerHand(playerHand);
-    if (playerScore >= highestHand) {
-      return false;
-    }
-  }
-  return true;
+  
+  return !otherPlayersCards.some((playerCards) => {
+    const playerHand = [...playerCards, ...allCommunityCards];
+    otherHand = evaluatePokerHand(playerHand);
+    return otherHand > highestHand
+  })
 }
 
 function buildPokerDeck(knownCards) {
